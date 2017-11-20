309f86c368b8d98a5274a7ee22617523317fc3a6
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 21:13:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,3 @@
-require 'pry'
-#
-# def reverse_each_word(string)
-#   array = string.split
-#   new_array = []
-#   array.each do |word|
-#       new_array << word.reverse
-#   end
-#   new_array.join(" ")
-# end
-#
-
-# string = "Hello there, and how are you?"
-
 def reverse_each_word(string)
   array = string.split
   new_array = array.collect do |word|
@@ -19,17 +5,3 @@ def reverse_each_word(string)
   end
   new_array.join(" ")
 end
-
-
-# reverse_each_word(string)
-
-#
-# def reverse_each_word(string)
-#   array = string.split
-#   new_array = []
-#   array.each do |word|
-#       new_array << word.reverse
-#   end
-#   new_array.join(" ")
-# end
-#
