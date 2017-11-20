62c1411f07b789b2a40b194a3ee4c64284de8b11
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 21:10:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -15,9 +15,9 @@ require 'pry'
 def reverse_each_word(string)
   array = string.split
   new_array = array.collect do |word|
-    word.reverse.join(" ")
+    word.reverse
   end
-  new_array
+  new_array.join(" ")
 end
 
 
